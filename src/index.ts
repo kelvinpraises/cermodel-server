81743147b4080cf758c1a22f24079cfb323c6e63
kelvinpraises <kelvinpraises@gmail.com>
2022-09-21 18:06:12 +0100

    added .js extensions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import deployModels from "./features/deploy-models";
-import { handleError } from "./utils/middleware";
+import deployModels from "./features/deploy-models/index.js";
+import { handleError } from "./utils/middleware.js";
 
 dotenv.config();
 
